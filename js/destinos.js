3afcfa62655f2b5f6af1e0cf32f11e5d8288a62e
LoreaH9 <l_hernandez@fpzornotza.com>
2021-10-06 11:09:28 +0200

    destinos.html creado y hecho

---
+++ js/destinos.js
@@ -0,0 +1,17 @@
+var lugares = ['Tokio','Shinjuku City','Fujinomiya','Sumida City','Kioto','Fujikawaguchiko','Oshino Hakkai','Taito City'];
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    loadPlaces();
+});
+
+function loadPlaces(){
+
+    lugares.forEach(element => {
+        console.log(element)
+        document.getElementById('places').innerHTML += "<div class='col-6 col-md-6 col-lg-4 g-3 '><div class='card background h-100 card-body p-5'><h5>"+element+"</h5><button class='btn btn-primary btn-rounded btn-sm'>171 Tours y actividades</button></div>"
+    });
+
+    for (let i = 0; i < document.getElementsByClassName('card').length; i++) {
+        document.getElementsByClassName("card")[i].style.backgroundImage = "url('../images/destinos/"+i+".png')";
+    }
+}
