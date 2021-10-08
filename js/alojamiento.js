42ce1e9f0f7e1fcc9bac092a0a13975c703408cc
kevino333 <74492727+kevino333@users.noreply.github.com>
2021-10-08 13:29:05 +0200

    Cambios en funcionamiento
    +Si al buscar no coincide con ningún hotel almacenado, se refleja en la pagina
    +El dropdown de orden tiene valor por defecto a parte de las opciones
    +Ahora ordena por precio, por ubicación todavía no funciona

--- js/alojamiento.js
+++ js/alojamiento.js
@@ -1,14 +1,15 @@
 hoteles = [
-  ["Shinjuku Granbell Hotel", "Descripcion del hotel Shinjuku", "Ubicacion del hotel Shinjuku", "840,99€", "images/Hoteles/HotelShinjukuGranbell.jpg"],
-  ["Red Planet Tokyo Asakusa", "Descripcion del hotel Red Planet", "Ubicacion del hotel Red Planet", "179,99€", "images/Hoteles/HotelRedPlanet.jpg"],
-  ["Mitsui Garden Hotel", "Descripcion del hotel Mtsui Garden", "Ubicacion del hotel Mitsui Garden", "420,69€", "images/Hoteles/HotelMitsuiGarden.jpg"],
-  ["Imaya Hostel Kyoto", "Descripcion del hotel Imaya Hostel", "Ubicacion del hotel Imaya Hostel", "21,95€", "images/Hoteles/HotelImayaHostel.jpg"]
+  ["Shinjuku Granbell Hotel", "Descripcion del hotel Shinjuku", "Shinjuku", "40,99€", "images/Hoteles/HotelShinjukuGranbell.jpg"],
+  ["Red Planet Tokyo Asakusa", "Descripcion del hotel Red Planet", "Tokyo Red Planet", "579,99€", "images/Hoteles/HotelRedPlanet.jpg"],
+  ["Mitsui Garden Hotel", "Descripcion del hotel Mtsui Garden", "AAA Mitsui Garden", "420,69€", "images/Hoteles/HotelMitsuiGarden.jpg"],
+  ["Imaya Hostel Kyoto", "Descripcion del hotel Imaya Hostel", "eee Imaya Hostel", "1,99€", "images/Hoteles/HotelImayaHostel.jpg"]
 ]
 
 document.addEventListener("DOMContentLoaded", function(event){
   ordenNombre=document.getElementById("ordenNombre");
   ordenPrecio=document.getElementById("ordenPrecio");
   ordenUbicacion=document.getElementById("ordenUbicacion");
+
 });
 
 window.onload = function () {
@@ -19,10 +20,12 @@ document.getElementById("input").addEventListener("keyup", function () { buscar(
 
 
 function buscar() {
+  var notFound = false;
   var respuesta = document.getElementById("input").value;
   document.getElementById("cardGroup").innerHTML = "";
   for (let i = 0; i < hoteles.length; i++) {
     if (hoteles[i][0].toLowerCase().includes(respuesta.toLowerCase())) {
+      notFound = true
       document.getElementById("cardGroup").innerHTML += `
                 <div class="card card-alojamiento col-lg-3 col-md-5 col-sm-8 col-8">
                   <a href="" class="cardTop ">
@@ -44,6 +47,9 @@ function buscar() {
                 </div>`;
     }
   }
+  if (notFound == false) {
+      document.getElementById("cardGroup").innerHTML += "<div id='notFound'> No se han encontrado hoteles! ＞﹏＜ </div>";
+    }
 }
 
 function ordenar(){
@@ -57,12 +63,17 @@ function ordenar(){
 
     console.log("precio seleccionado")
     hoteles.sort(function(a, b){
-      return a[3] - b[3]
+    console.log(parseInt(a[3]) - parseInt(b[3]))
+      return parseInt(a[3]) - parseInt(b[3])
     })
     console.log(hoteles)
     buscar();
     
   } else if (ordenUbicacion.selected == true){
     console.log("ubicacion seleccionado")
+
+    hoteles.sort(function(a, b){
+      return (a[2]) - (b[2])
+    })
   }
 }
