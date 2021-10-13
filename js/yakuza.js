f9dfa9b4513f703e9d4b6bb3f402524a5f9484f3
mugira8 <74660528+mugira8@users.noreply.github.com>
2021-10-13 08:53:58 +0200

    Cambio de Yakuza a Kabukicho

--- js/yakuza.js
+++ js/yakuza.js
@@ -2,7 +2,12 @@ let origen = document.getElementById("1");
 let actividades = document.getElementById("2");
 let otro = document.getElementById("3");
 let advertencia = document.getElementById("4");
-let titulo = document.getElementById("titulo")
+let titulo = document.getElementById("titulo");
+let kabukicho = document.getElementById("0");
+
+kabukicho.addEventListener("click", function(){
+    titulo.innerHTML="-- Kabukicho --";
+})
 
 
 origen.addEventListener("click", function(){
