8fe211e4c43f5c53a2d4b8712c453544cf2487ac
LoreaH9 <l_hernandez@fpzornotza.com>
2021-10-13 10:19:02 +0200

    cambios planificacion e index

--- js/planificacion.js
+++ js/planificacion.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function loadGallery(){
-    for (let i=0; i<6; i++) {
-        document.getElementById("gallery").innerHTML += " <div class='col-lg-4 col-md-12 text-center'><img src='images/planificacion/"+i+".png' class='w-100 shadow-1-strong rounded mb-4'/></div>";
+    for (let i=0; i<12; i++) {
+        document.getElementById("gallery").innerHTML += " <div class='col-lg-4 col-md-12 text-center'><img src='images/planificacion/"+i+".png' class='w-100 shadow-1-strong rounded mb-4' height='90%'/></div>";
     }
 }
