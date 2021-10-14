5d8cf499c30be86e52cb7359d141a542f33778f2
LoreaH9 <l_hernandez@fpzornotza.com>
2021-10-14 10:24:29 +0200

    actualizado

--- js/destinos.js
+++ js/destinos.js
@@ -8,7 +8,7 @@ function loadPlaces(){
 
     lugares.forEach(element => {
         console.log(element)
-        document.getElementById('places').innerHTML += "<div class='col-6 col-md-6 col-lg-4 g-3 '><div class='card background h-100 card-body p-5'><h5>"+element+"</h5><button class='btn btn-primary type='button' data-bs-toggle='modal' data-bs-target='#exampleModal' btn-rounded btn-sm'>"+Math.floor(Math.random() * 200)+" Tours y actividades</button></div>"
+        document.getElementById('places').innerHTML += "<div class='col-6 col-md-6 col-lg-4 g-3 '><div class='card background h-100 card-body p-5'><h5>"+element+"</h5><button class='btn btn-primary type='button' data-bs-toggle='offcanvas' data-bs-target='#exampleModal' btn-rounded btn-sm'>"+Math.floor(Math.random() * 200)+" Tours y actividades</button></div>"
     });
 
     for (let i = 0; i < document.getElementsByClassName('card').length; i++) {
