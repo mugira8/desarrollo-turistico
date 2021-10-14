72b6bcf7fc5a71cff488b1b7d4daa8313503fd97
mugira8 <74660528+mugira8@users.noreply.github.com>
2021-10-14 09:32:49 +0200

    adriel

--- js/yakuza.js
+++ js/yakuza.js
@@ -4,24 +4,71 @@ let otro = document.getElementById("3");
 let advertencia = document.getElementById("4");
 let titulo = document.getElementById("titulo");
 let kabukicho = document.getElementById("0");
+let titulocard1 = document.getElementById("card1-title");
+let textcard1 = document.getElementById("card1-text");
+let imgcard1 = document.getElementById("card1-img")
+let titulocard2 = document.getElementById("card2-title");
+let textcard2 = document.getElementById("card2-text");
+let imgcard2 = document.getElementById("card2-img");
 
 kabukicho.addEventListener("click", function(){
-    titulo.innerHTML="-- Kabukicho --";
+    titulo.innerHTML=" Kabukicho ";
+    
+    titulocard1.innerHTML="CARACTERISTICAS";
+    textcard1.innerHTML="Kabukichō (歌舞伎町?) es un vecindario de Shinjuku, Tokio, Japón. Se considera el barrio rojo más importante de Japón; en su entorno se encuentran miles de locales de entretenimiento enfocados al mercado de adultos. El área cuenta con muchos bares, hostales, moteles, locales comerciales, restaurantes y clubes nocturnos tanto para mujeres como para hombres. En la parte oeste del vecindario se hallan también muchos de los establecimientos dedicados a la población gay de Tokio, que se extiende hasta el barrio de Shinjuku San-Choume (新宿三丁目).";
+    imgcard1.src="./images/kabukicho_card.jpg";
+
+    titulocard2.innerHTML="LA ZONA";
+    textcard2.innerHTML="Shinjuku es la estación de tren más bulliciosa de Japón ¡y puede que del mundo entero! Al oeste existe un distrito de oficinas en cuyo centro se encuentran las dependencias gubernamentales metropolitanas de Tokio. Al este, un barrio con centros comerciales de larga trayectoria, cines y teatros. El parque Shinjuku Gyoen, que fue en su origen jardín de la casa imperial, es muy célebre como lugar de contemplación de las flores del cerezo en marzo y abril. En Shinjuku, muchos restaurantes se mantienen abiertos toda la noche y, en distritos como Omoide Yokocho (o “callejón de los recuerdos”) o Shinjuku Golden Machi, las calles secundarias están llenas de diminutos restaurantes y bares que reflejan el estilo y la personalidad únicos de cada dueño.";
+    imgcard2.src="./images/shinjuku.jpg";
 })
 
 
 origen.addEventListener("click", function(){
-    titulo.innerHTML="-- Origen --";
+    titulo.innerHTML=" Origen ";
+
+    titulocard1.innerHTML="CARACTERISTICAS";
+    textcard1.innerHTML="Kabukichō (歌舞伎町?) es un vecindario de Shinjuku, Tokio, Japón. Se considera el barrio rojo más importante de Japón; en su entorno se encuentran miles de locales de entretenimiento enfocados al mercado de adultos. El área cuenta con muchos bares, hostales, moteles, locales comerciales, restaurantes y clubes nocturnos tanto para mujeres como para hombres. En la parte oeste del vecindario se hallan también muchos de los establecimientos dedicados a la población gay de Tokio, que se extiende hasta el barrio de Shinjuku San-Choume (新宿三丁目).";
+    imgcard1.src="./images/kabukicho_card.jpg";
+
+    titulocard2.innerHTML="LA ZONA";
+    textcard2.innerHTML="Shinjuku es la estación de tren más bulliciosa de Japón ¡y puede que del mundo entero! Al oeste existe un distrito de oficinas en cuyo centro se encuentran las dependencias gubernamentales metropolitanas de Tokio. Al este, un barrio con centros comerciales de larga trayectoria, cines y teatros. El parque Shinjuku Gyoen, que fue en su origen jardín de la casa imperial, es muy célebre como lugar de contemplación de las flores del cerezo en marzo y abril. En Shinjuku, muchos restaurantes se mantienen abiertos toda la noche y, en distritos como Omoide Yokocho (o “callejón de los recuerdos”) o Shinjuku Golden Machi, las calles secundarias están llenas de diminutos restaurantes y bares que reflejan el estilo y la personalidad únicos de cada dueño.";
+    imgcard2.src="./images/shinjuku.jpg";
 })
 
 actividades.addEventListener("click", function(){
-    titulo.innerHTML="-- Actividades --";
+    titulo.innerHTML=" Actividades ";
+
+    titulocard1.innerHTML="CARACTERISTICAS";
+    textcard1.innerHTML="Kabukichō (歌舞伎町?) es un vecindario de Shinjuku, Tokio, Japón. Se considera el barrio rojo más importante de Japón; en su entorno se encuentran miles de locales de entretenimiento enfocados al mercado de adultos. El área cuenta con muchos bares, hostales, moteles, locales comerciales, restaurantes y clubes nocturnos tanto para mujeres como para hombres. En la parte oeste del vecindario se hallan también muchos de los establecimientos dedicados a la población gay de Tokio, que se extiende hasta el barrio de Shinjuku San-Choume (新宿三丁目).";
+    imgcard1.src="./images/kabukicho_card.jpg";
+
+    titulocard2.innerHTML="LA ZONA";
+    textcard2.innerHTML="Shinjuku es la estación de tren más bulliciosa de Japón ¡y puede que del mundo entero! Al oeste existe un distrito de oficinas en cuyo centro se encuentran las dependencias gubernamentales metropolitanas de Tokio. Al este, un barrio con centros comerciales de larga trayectoria, cines y teatros. El parque Shinjuku Gyoen, que fue en su origen jardín de la casa imperial, es muy célebre como lugar de contemplación de las flores del cerezo en marzo y abril. En Shinjuku, muchos restaurantes se mantienen abiertos toda la noche y, en distritos como Omoide Yokocho (o “callejón de los recuerdos”) o Shinjuku Golden Machi, las calles secundarias están llenas de diminutos restaurantes y bares que reflejan el estilo y la personalidad únicos de cada dueño.";
+    imgcard2.src="./images/shinjuku.jpg";
 })
 
 otro.addEventListener("click", function(){
-    titulo.innerHTML="-- Otro --";
+    titulo.innerHTML=" Otras ubicaciones ";
+
+    titulocard1.innerHTML="CARACTERISTICAS";
+    textcard1.innerHTML="Kabukichō (歌舞伎町?) es un vecindario de Shinjuku, Tokio, Japón. Se considera el barrio rojo más importante de Japón; en su entorno se encuentran miles de locales de entretenimiento enfocados al mercado de adultos. El área cuenta con muchos bares, hostales, moteles, locales comerciales, restaurantes y clubes nocturnos tanto para mujeres como para hombres. En la parte oeste del vecindario se hallan también muchos de los establecimientos dedicados a la población gay de Tokio, que se extiende hasta el barrio de Shinjuku San-Choume (新宿三丁目).";
+    imgcard1.src="./images/kabukicho_card.jpg";
+
+    titulocard2.innerHTML="LA ZONA";
+    textcard2.innerHTML="Shinjuku es la estación de tren más bulliciosa de Japón ¡y puede que del mundo entero! Al oeste existe un distrito de oficinas en cuyo centro se encuentran las dependencias gubernamentales metropolitanas de Tokio. Al este, un barrio con centros comerciales de larga trayectoria, cines y teatros. El parque Shinjuku Gyoen, que fue en su origen jardín de la casa imperial, es muy célebre como lugar de contemplación de las flores del cerezo en marzo y abril. En Shinjuku, muchos restaurantes se mantienen abiertos toda la noche y, en distritos como Omoide Yokocho (o “callejón de los recuerdos”) o Shinjuku Golden Machi, las calles secundarias están llenas de diminutos restaurantes y bares que reflejan el estilo y la personalidad únicos de cada dueño.";
+    imgcard2.src="./images/shinjuku.jpg";
+
 })
 
 advertencia.addEventListener("click", function(){
-    titulo.innerHTML="-- Advertenvcia --";
+    titulo.innerHTML=" Advertenvcia ";
+
+    titulocard1.innerHTML="CARACTERISTICAS";
+    textcard1.innerHTML="Kabukichō (歌舞伎町?) es un vecindario de Shinjuku, Tokio, Japón. Se considera el barrio rojo más importante de Japón; en su entorno se encuentran miles de locales de entretenimiento enfocados al mercado de adultos. El área cuenta con muchos bares, hostales, moteles, locales comerciales, restaurantes y clubes nocturnos tanto para mujeres como para hombres. En la parte oeste del vecindario se hallan también muchos de los establecimientos dedicados a la población gay de Tokio, que se extiende hasta el barrio de Shinjuku San-Choume (新宿三丁目).";
+    imgcard1.src="./images/kabukicho_card.jpg";
+
+    titulocard2.innerHTML="LA ZONA";
+    textcard2.innerHTML="Shinjuku es la estación de tren más bulliciosa de Japón ¡y puede que del mundo entero! Al oeste existe un distrito de oficinas en cuyo centro se encuentran las dependencias gubernamentales metropolitanas de Tokio. Al este, un barrio con centros comerciales de larga trayectoria, cines y teatros. El parque Shinjuku Gyoen, que fue en su origen jardín de la casa imperial, es muy célebre como lugar de contemplación de las flores del cerezo en marzo y abril. En Shinjuku, muchos restaurantes se mantienen abiertos toda la noche y, en distritos como Omoide Yokocho (o “callejón de los recuerdos”) o Shinjuku Golden Machi, las calles secundarias están llenas de diminutos restaurantes y bares que reflejan el estilo y la personalidad únicos de cada dueño.";
+    imgcard2.src="./images/shinjuku.jpg";
 })
