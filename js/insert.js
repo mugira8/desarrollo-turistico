7cc06969d469fd25d7d230781d42e364a05712c7
Adriel184 <74302576+Adriel184@users.noreply.github.com>
2021-10-19 12:10:09 +0200

    Navbar y footer

---
+++ js/insert.js
@@ -0,0 +1,7 @@
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function(){
+    $(function(){
+        $("#navbar").load("navbar.html");
+        $("#footer").load("footer.html"); 
+    });
+});
